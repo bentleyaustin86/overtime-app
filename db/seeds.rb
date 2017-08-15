6bf0e613994ef570f33f2ee8cfd920142fd846c3
bentleyaustin86 <bentleyaustin86@gmail.com>
2017-08-15 10:08:07 -0600

    updated seed file, implemented index view with posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-1.times do |user|
-  User.create!(first_name: “Austin”, last_name: “Bentley”, email: “test@test.com”, password: “test!!”, password_confirmation: “test!!”)
-end
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+
+puts "1 User created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
