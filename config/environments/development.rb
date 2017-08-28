6a3c61e8b913ae38c102884ce27f34ac983d5ee0
bentleyaustin86 <bentleyaustin86@gmail.com>
2017-08-28 14:49:16 -0600

    Integrated basic manager notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,45 +1,40 @@
+# Rails.application.configure do
+#   config.cache_classes = false
+#   config.eager_load = false
+#   config.consider_all_requests_local = true
+#   if Rails.root.join('tmp/caching-dev.txt').exist?
+#     config.action_controller.perform_caching = true
+#     config.cache_store = :memory_store
+#     config.public_file_server.headers = {
+#       'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+#     }
+#   else
+#     config.action_controller.perform_caching = false
+#     config.cache_store = :null_store
+#   end
+
+#   config.action_mailer.raise_delivery_errors = false
+#   config.action_mailer.perform_caching = false
+#   config.active_support.deprecation = :log
+#   config.active_record.migration_error = :page_load
+#   config.assets.debug = true
+#   config.assets.quiet = true
+#   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+#   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+#   config.action_mailer.perform_deliveries = true
+#   config.action_mailer.raise_delivery_errors = true
+# end
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  # Do not eager load code on boot.
   config.eager_load = false
-
-  # Show full error reports.
-  config.consider_all_requests_local = true
-
-  # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
-
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
   config.active_support.deprecation = :log
-
   config.active_record.migration_error = :page_load
-
- 
   config.assets.debug = true
-
-  config.assets.quiet = true
-
-
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.assets.digest = true
+  config.assets.raise_runtime_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
